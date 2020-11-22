506aaf4d785e247bb38ae1623daa7e9563715c90
Abhinav Tushar <abhinav@lepisma.xyz>
2020-11-22 16:07:30 +0530

    stats fns

--- akku/cli.py
+++ akku/cli.py
@@ -2,19 +2,21 @@
 Akku
 
 Usage:
-  akku plot entries [--orgzly-file=<orgzly-file>] [--org-journal-dir=<org-journal-dir>]
-    [--org-list-file=<org-list-file>]
+  akku stats [--orgzly-file=<orgzly-file>] [--org-journal-dir=<org-journal-dir>]
+    [--org-list-file=<org-list-file>] [--output-file=<output-file>]
 
 Options:
   --orgzly-file=<orgzly-file>           Orgzly style log file.
   --org-journal-dir=<org-journal-dir>   Org journal directory.
   --org-list-file=<org-list-file>       File with list entry items.
+  --output-file=<output-file>           Output file to put stats in [default: stats.csv].
 """
 
 import getpass
 
 from docopt import docopt
 
+import akku.stats as stats
 from akku import __version__
 from akku.parser import parse_list_journal, parse_org_journal, parse_orgzly
 
@@ -29,12 +31,17 @@ def main():
             entries.extend(parse_orgzly(args["--orgzly-file"]))
 
         if args["--org-journal-dir"]:
-            passphrase = getpass.getpass()
+            passphrase = getpass.getpass("Passphrase for Org Journal: ")
             entries.extend(parse_org_journal(args["--org-journal-dir"], passphrase))
 
         if args["--org-list-file"]:
             entries.extend(parse_list_journal(args["--org-list-file"]))
 
-        print(entries[-1])
+        df = stats.summarize_discrete_moods(entries).merge(
+            stats.summarize_number_of_entries(entries),
+            how="outer",
+            on="day"
+        )
+        df.to_csv(args["--output-file"], index=None)
     else:
         raise RuntimeError("Need at least one source to work on")
---
+++ akku/stats.py
@@ -0,0 +1,62 @@
+import datetime
+import statistics
+from typing import List
+
+import pandas as pd
+from pydash import py_
+
+from akku.types import Entry
+
+
+def summarize_number_of_entries(entries: List[Entry]):
+    current_year = datetime.datetime.now().year()
+    filtered = [e for e in entries if e.date.year == current_year]
+
+    df = []
+    for date, entries in py_.group_by(filtered, lambda e: e.date).items():
+        df.append({
+            "day": date.timetuple().tm_yday,
+            "value": len(entries)
+        })
+
+    return pd.DataFrame(df)
+
+
+def summarize_number_of_mentions(entries: List[Entry], name: str):
+    """
+    Number of mention of a person. Matches by using nomie thing only. Regex
+    match will give more real stats for older dates.
+    """
+
+    current_year = datetime.datetime.now().year()
+    filtered = [e for e in entries if e.date.year == current_year]
+
+    df = []
+    for date, entries in py_.group_by(filtered, lambda e: e.date).items():
+        people = py_.flatten([e.people for e in entries])
+        mentions = [p for p in people if p.name == name]
+
+        if mentions:
+            df.append({
+                "day": date.timetuple().tm_yday,
+                "value": len(mentions)
+            })
+
+    return pd.DataFrame(df)
+
+
+def summarize_discrete_moods(entries: List[Entry]):
+    current_year = datetime.datetime.now().year()
+    filtered = [e for e in entries if e.date.year == current_year]
+
+    df = []
+    for date, entries in py_.group_by(filtered, lambda e: e.date).items():
+        trackers = py_.flatten([e.trackers for e in entries])
+        mood_trackers = [t for t in trackers if t.name == "mood"]
+        if mood_trackers:
+            df.append({
+                "day": date.timetuple().tm_yday,
+                "value": round(statistics.mean([t.value for t in mood_trackers]))
+            })
+
+    return pd.DataFrame(df)
