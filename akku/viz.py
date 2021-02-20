949d623ff88be2f3a550d800da7bc1cd68bd5ed0
Abhinav Tushar <abhinav@lepisma.xyz>
2021-02-21 05:14:57 +0530

    better calendar plot

--- akku/viz.py
+++ akku/viz.py
@@ -2,6 +2,7 @@ import calendar
 import datetime
 from typing import Dict
 
+import matplotlib.colors
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
@@ -15,8 +16,15 @@ def plot_year(year: int, colors: Dict[datetime.date, str]):
         month = i + 1
         plot_month(axs[i // 3, i % 3], year, month, colors)
 
-    fig.suptitle(f"{year}", color="#999999", fontfamily="Lora", fontstyle="italic", fontsize="x-large", x=0.9, y=0.95, ha="right")
+    fig.suptitle(f"{year}", color="#999999", fontfamily="Lora", fontstyle="italic", fontsize="x-large", x=0.9, y=0.05, ha="right")
     plt.show()
+
+
+def dark_foreground(c) -> bool:
+    r, g, b, a = matplotlib.colors.to_rgba(c)
+    brightness = r * 0.299 + g * 0.587 + b * 0.114 + (1 - a)
+
+    return brightness > (186 / 255)
 
 
 def plot_month(ax: Axes, year: int, month: int, colors: Dict[datetime.date, str]):
@@ -28,7 +36,7 @@ def plot_month(ax: Axes, year: int, month: int, colors: Dict[datetime.date, str]
 
     cal = calendar.Calendar(firstweekday=6)
 
-    padding = 0.1
+    padding = 0.0
     for i, day in enumerate(["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]):
         ax.text(i + 1.5, 8 + 0.3, day, ha="center", color="#aaaaaa", fontfamily="Lora", fontstyle="italic", fontsize="smaller")
 
@@ -48,14 +56,11 @@ def plot_month(ax: Axes, year: int, month: int, colors: Dict[datetime.date, str]
         side = 1 - 2 * padding
 
         color = colors.get(dt, "white")
+        text_color = "#777777" if dark_foreground(color) else "white"
 
-        rect = patches.FancyBboxPatch((x, y), side, side, boxstyle="Round, pad=0, rounding_size=0.02", facecolor=color, edgecolor="#eeeeee")
+        rect = patches.FancyBboxPatch((x, y), side, side, boxstyle="Round, pad=0, rounding_size=0.0", facecolor=color, edgecolor="#eeeeee")
         ax.add_patch(rect)
-
-        rect = patches.FancyBboxPatch((x_grid + 1 - side / 2, y_grid), side / 2, side / 2, boxstyle="Round, pad=0, rounding_size=0.05", color="white")
-        ax.add_patch(rect)
-
-        ax.text(x_grid + 1 - side / 4, y_grid + side / 4, f"{dt.day}", ha="center", va="center", color="#999999", fontfamily="Lora", fontstyle="italic", fontsize="smaller")
+        ax.text(x + side / 2, y + side / 2, f"{dt.day}", ha="center", va="center", color=text_color, fontfamily="Lora", fontsize="x-small")
 
         if weekday == 6:
             row += 1
