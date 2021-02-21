32fe7190bf013801d7f091b2083c1fbfcf8c6ec5
Abhinav Tushar <abhinav@lepisma.xyz>
2021-02-21 20:59:59 +0530

    controls

--- akku/ui.py
+++ akku/ui.py
@@ -8,7 +8,8 @@ import numpy as np
 import orgpython
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QHBoxLayout, QSplitter, QTextEdit, QWidget
+from PyQt5.QtWidgets import (QComboBox, QHBoxLayout, QSplitter, QTextEdit,
+                             QToolButton, QVBoxLayout, QWidget)
 
 import akku.stats as stats
 import akku.viz as viz
@@ -23,7 +24,7 @@ class QJournal(QTextEdit):
     def __init__(self):
         super().__init__()
         self.setReadOnly(True)
-        self.setStyleSheet("QTextEdit { padding:10; border: none; }")
+        self.setStyleSheet("QTextEdit { padding:10; border: none; background-color: transparent; }")
         self.font_family = "Lora"
 
     def format_entry_dt(self, entry: Entry) -> str:
@@ -62,7 +63,7 @@ class QCalendar(FigureCanvasQTAgg):
         super().__init__(self.fig)
         self.entries = entries
 
-    def render(self, year: int, plot_type: str, **kwargs):
+    def render(self, year: int, plot_type: str):
         self.fig.clear()
 
         colors = {}
@@ -90,6 +91,7 @@ class QCalendar(FigureCanvasQTAgg):
                 colors[dt] = ct(v)
 
         viz.plot_year(self.fig, year, colors)
+        self.fig.canvas.draw_idle()
 
 
 class QWindow(QWidget):
@@ -100,19 +102,63 @@ class QWindow(QWidget):
     def __init__(self, entries):
         super().__init__()
         self.setWindowTitle("akku")
-        self.setStyleSheet("background-color: white;")
+        # self.setStyleSheet("background-color: white;")
 
         layout = QHBoxLayout()
+        self.year = datetime.datetime.now().year
+        self.plot_type = "mood"
+
         self.calendar = QCalendar(entries)
-        year = datetime.datetime.now().year
-        self.calendar.render(year, "mood")
+        self.refresh_calendar()
 
         self.journal = QJournal()
         self.journal.render(entries)
 
+        side_pane = QWidget()
+        side_pane_layout = QVBoxLayout()
+
+        controls = QWidget()
+        controls_layout = QHBoxLayout()
+
+        left_button = QToolButton()
+        left_button.setArrowType(Qt.LeftArrow)
+        left_button.clicked.connect(self.left_click)
+
+        right_button = QToolButton()
+        right_button.setArrowType(Qt.RightArrow)
+        right_button.clicked.connect(self.right_click)
+
+        self.combo_box = QComboBox()
+        self.combo_box.addItems(["mood", "count", "polarity", "mentions"])
+        self.combo_box.activated.connect(self.combo_click)
+
+        controls_layout.addWidget(left_button)
+        controls_layout.addWidget(right_button)
+        controls_layout.addWidget(self.combo_box)
+        controls.setLayout(controls_layout)
+
+        side_pane_layout.addWidget(controls)
+        side_pane_layout.addWidget(self.journal)
+        side_pane.setLayout(side_pane_layout)
+
         splitter = QSplitter(Qt.Horizontal)
         splitter.addWidget(self.calendar)
-        splitter.addWidget(self.journal)
+        splitter.addWidget(side_pane)
 
         layout.addWidget(splitter)
         self.setLayout(layout)
+
+    def refresh_calendar(self):
+        self.calendar.render(self.year, self.plot_type)
+
+    def left_click(self):
+        self.year -= 1
+        self.refresh_calendar()
+
+    def right_click(self):
+        self.year += 1
+        self.refresh_calendar()
+
+    def combo_click(self):
+        self.plot_type = self.combo_box.currentText()
+        self.refresh_calendar()
